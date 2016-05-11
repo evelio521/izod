3d5935c79044f2943787f4b35af0c286fbeaa06d
sunqiang <qiangsun@mobvoi.com>
2016-05-11 15:21:40 +0800

    add client

--- src/client.cc
+++ src/client.cc
@@ -23,11 +23,8 @@
 #include "base/log.h"
 #include "base/string_util.h"
 #include "thirdlibs/gflags/gflags.h"
-#include "thirdlibs/event/include/event2/event.h"
-#include "thirdlibs/event/include/event2/buffer.h"
-#include "thirdlibs/event/include/event2/http.h"
-#include "thirdlibs/event/include/event2/http_struct.h"
-#include "thirdlibs/event/include/event2/keyvalq_struct.h"
+#include "thirdlibs/glog/logging.h"
+
 
 DEFINE_int32(max_head_buffer_size, 20 * 1024, "");
 DEFINE_int32(max_body_buffer_size, 2 * 1024 * 1024, "");
@@ -74,105 +71,31 @@ struct Buffer {
   string data;
 };
 
+/************************** Ahead Declare ******************************/
+static void http_requset_post_cb(struct evhttp_request *req, void *arg);
+static void http_requset_get_cb(struct evhttp_request *req, void *arg);
+static int start_url_request(struct http_request_get *http_req, int req_get_flag);
+
 /************************** Tools Function ******************************/
-void Client::print_request_head_info(struct evkeyvalq *header) {
+static void print_request_head_info(struct evkeyvalq *header) {
   struct evkeyval *first_node = header->tqh_first;
   while (first_node) {
-//        MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"key:%s  value:%s", first_node->key, first_node->value);
+    LOG(INFO) << "key:" << first_node->key <<" value:" << first_node->value;
     first_node = first_node->next.tqe_next;
   }
 }
 
-void Client::print_uri_parts_info(const struct evhttp_uri * http_uri){
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"scheme:%s", evhttp_uri_get_scheme(http_uri));
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"host:%s", evhttp_uri_get_host(http_uri));
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"path:%s", evhttp_uri_get_path(http_uri));
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"port:%d", evhttp_uri_get_port(http_uri));
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"query:%s", evhttp_uri_get_query(http_uri));
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"userinfo:%s", evhttp_uri_get_userinfo(http_uri));
-//    MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"fragment:%s", evhttp_uri_get_fragment(http_uri));
+static void print_uri_parts_info(const struct evhttp_uri * http_uri) {
+  LOG(INFO) << "scheme:" << evhttp_uri_get_scheme(http_uri);
+  LOG(INFO) << "host:" << evhttp_uri_get_host(http_uri);
+  LOG(INFO) << "path:" << evhttp_uri_get_path(http_uri);
+  LOG(INFO) << "port:" << evhttp_uri_get_port(http_uri);
+  LOG(INFO) << "query:" << evhttp_uri_get_query(http_uri);
+  LOG(INFO) << "userinfo:" << evhttp_uri_get_userinfo(http_uri);
+  LOG(INFO) << "fragment:" << evhttp_uri_get_fragment(http_uri);
 }
 
-/************************** Request Function ******************************/
-void Client::http_requset_post_cb(struct evhttp_request *req, void *arg) {
-  struct http_request_post *http_req_post = (struct http_request_post *) arg;
-  switch (req->response_code) {
-    case HTTP_OK: {
-      struct evbuffer* buf = evhttp_request_get_input_buffer(req);
-      size_t len = evbuffer_get_length(buf);
-//            MITLog_DetPuts(MITLOG_LEVEL_COMMON, "print the head info:");
-      print_request_head_info(req->output_headers);
-
-//            MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"len:%zu  body size:%zu", len, req->body_size);
-      char *tmp = malloc(len + 1);
-      memcpy(tmp, evbuffer_pullup(buf, -1), len);
-      tmp[len] = '\0';
-//            MITLog_DetPuts(MITLOG_LEVEL_COMMON, "print the body:");
-//            MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"HTML BODY:%s", tmp);
-      free(tmp);
-
-      event_base_loopexit(http_req_post->base, 0);
-      break;
-    }
-    case HTTP_MOVEPERM:
-//            MITLog_DetPrintf(MITLOG_LEVEL_ERROR, "%s", "the uri moved permanently");
-      break;
-    case HTTP_MOVETEMP: {
-      const char *new_location = evhttp_find_header(req->input_headers,
-                                                    "Location");
-      struct evhttp_uri *new_uri = evhttp_uri_parse(new_location);
-      evhttp_uri_free(http_req_post->uri);
-      http_req_post->uri = new_uri;
-      start_url_request((struct http_request_get *) http_req_post,
-                        EVHTTP_REQ_POST);
-      return;
-    }
-
-    default:
-      event_base_loopexit(http_req_post->base, 0);
-      return;
-  }
-}
-void Client::http_requset_get_cb(struct evhttp_request *req, void *arg) {
-  struct http_request_get *http_req_get = (struct http_request_get *) arg;
-  switch (req->response_code) {
-    case HTTP_OK: {
-      struct evbuffer* buf = evhttp_request_get_input_buffer(req);
-      size_t len = evbuffer_get_length(buf);
-//            MITLog_DetPuts(MITLOG_LEVEL_COMMON, "print the head info:");
-      print_request_head_info(req->output_headers);
-
-//            MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"len:%zu  body size:%zu", len, req->body_size);
-      char *tmp = malloc(len + 1);
-      memcpy(tmp, evbuffer_pullup(buf, -1), len);
-      tmp[len] = '\0';
-//            MITLog_DetPuts(MITLOG_LEVEL_COMMON, "print the body:");
-//            MITLog_DetPrintf(MITLOG_LEVEL_COMMON,"HTML BODY:%s", tmp);
-      free(tmp);
-
-      event_base_loopexit(http_req_get->base, 0);
-      break;
-    }
-    case HTTP_MOVEPERM:
-//            MITLog_DetPrintf(MITLOG_LEVEL_ERROR, "%s", "the uri moved permanently");
-      break;
-    case HTTP_MOVETEMP: {
-      const char *new_location = evhttp_find_header(req->input_headers,
-                                                    "Location");
-      struct evhttp_uri *new_uri = evhttp_uri_parse(new_location);
-      evhttp_uri_free(http_req_get->uri);
-      http_req_get->uri = new_uri;
-      start_url_request(http_req_get, EVHTTP_REQ_GET);
-      return;
-    }
-
-    default:
-      event_base_loopexit(http_req_get->base, 0);
-      return;
-  }
-}
-
-int Client::start_url_request(struct http_request_get *http_req,
+static int start_url_request(struct http_request_get *http_req,
                               int req_get_flag) {
   if (http_req->cn)
     evhttp_connection_free(http_req->cn);
@@ -195,8 +118,9 @@ int Client::start_url_request(struct http_request_get *http_req,
 
   if (req_get_flag == EVHTTP_REQ_POST) {
     const char *path = evhttp_uri_get_path(http_req->uri);
+    string path_post = path ? path : "/";
     evhttp_make_request(http_req->cn, http_req->req, EVHTTP_REQ_POST,
-                        path ? path : "/");
+                        reinterpret_cast<const char*>(path_post.c_str()));
     /** Set the post data */
     struct http_request_post *http_req_post =
         (struct http_request_post *) http_req;
@@ -210,7 +134,7 @@ int Client::start_url_request(struct http_request_get *http_req,
     size_t len = (query ? strlen(query) : 0) + (path ? strlen(path) : 0) + 1;
     char *path_query = NULL;
     if (len > 1) {
-      path_query = calloc(len, sizeof(char));
+      path_query = (char*)calloc(len, sizeof(char));
       sprintf(path_query, "%s?%s", path, query);
     }
     evhttp_make_request(http_req->cn, http_req->req, EVHTTP_REQ_GET,
@@ -221,6 +145,85 @@ int Client::start_url_request(struct http_request_get *http_req,
                     evhttp_uri_get_host(http_req->uri));
 
   return 0;
+}
+
+/************************** Request Function ******************************/
+static void http_requset_post_cb(struct evhttp_request *req, void *arg) {
+  struct http_request_post *http_req_post = (struct http_request_post *) arg;
+  switch (req->response_code) {
+    case HTTP_OK: {
+      struct evbuffer* buf = evhttp_request_get_input_buffer(req);
+      size_t len = evbuffer_get_length(buf);
+      LOG(INFO) << "print the head info:";
+      print_request_head_info(req->output_headers);
+
+      LOG(INFO) << "len :" << len << " body size" << req->body_size;
+      char* tmp = (char* )malloc(len + 1);
+      memcpy(tmp, evbuffer_pullup(buf, -1), len);
+      tmp[len] = '\0';
+      LOG(INFO) <<"print the body:";
+      LOG(INFO) << "HTML BODY:" <<  tmp;
+      free(tmp);
+
+      event_base_loopexit(http_req_post->base, 0);
+      break;
+    }
+    case HTTP_MOVEPERM:
+      LOG(INFO) << "the uri moved permanently";
+      break;
+    case HTTP_MOVETEMP: {
+      const char *new_location = evhttp_find_header(req->input_headers,
+                                                    "Location");
+      struct evhttp_uri *new_uri = evhttp_uri_parse(new_location);
+      evhttp_uri_free(http_req_post->uri);
+      http_req_post->uri = new_uri;
+      start_url_request((struct http_request_get *) http_req_post,
+                        EVHTTP_REQ_POST);
+      return;
+    }
+
+    default:
+      event_base_loopexit(http_req_post->base, 0);
+      return;
+  }
+}
+static void http_requset_get_cb(struct evhttp_request *req, void *arg) {
+  struct http_request_get *http_req_get = (struct http_request_get *) arg;
+  switch (req->response_code) {
+    case HTTP_OK: {
+      struct evbuffer* buf = evhttp_request_get_input_buffer(req);
+      size_t len = evbuffer_get_length(buf);
+      LOG(INFO) << "print the head info:";
+      print_request_head_info(req->output_headers);
+
+      LOG(INFO) << "len :" << len << " body size" << req->body_size;
+      char *tmp = (char *)malloc(len + 1);
+      memcpy(tmp, evbuffer_pullup(buf, -1), len);
+      tmp[len] = '\0';
+      LOG(INFO) <<"print the body:";
+      LOG(INFO) << "HTML BODY:" <<  tmp;
+      free(tmp);
+
+      event_base_loopexit(http_req_get->base, 0);
+      break;
+    }
+    case HTTP_MOVEPERM:
+      LOG(INFO) << "the uri moved permanently";
+      break;
+    case HTTP_MOVETEMP: {
+      const char *new_location = evhttp_find_header(req->input_headers,
+                                                    "Location");
+      struct evhttp_uri *new_uri = evhttp_uri_parse(new_location);
+      evhttp_uri_free(http_req_get->uri);
+      http_req_get->uri = new_uri;
+      start_url_request(http_req_get, EVHTTP_REQ_GET);
+      return;
+    }
+
+    default:
+      event_base_loopexit(http_req_get->base, 0);
+      return;
+  }
 }
 
 /************************** New/Free Function ******************************/
@@ -240,7 +243,7 @@ void* Client::http_request_new(struct event_base* base,
     len = sizeof(struct http_request_post);
   }
 
-  struct http_request_get *http_req_get = calloc(1, len);
+  struct http_request_get *http_req_get = (client::http_request_get*)calloc(1, len);
   http_req_get->uri = evhttp_uri_parse(url);
   print_uri_parts_info(http_req_get->uri);
 
@@ -288,30 +291,40 @@ void Client::http_request_free(struct http_request_get *http_req_get,
 /**
  * @param content_type: refer HTTP_CONTENT_TYPE_*
  */
-void* Client::start_http_requset(struct event_base* base,
+bool Client::start_http_requset(struct event_base* base,
                                 const char *url,
                                 int req_get_flag,
                                 const char *content_type,
                                 const char* data) {
-  struct http_request_get *http_req_get = http_request_new(base, url,
+  struct http_request_get *http_req_get = (struct http_request_get*)http_request_new(base, url,
                                                            req_get_flag,
                                                            content_type, data);
   start_url_request(http_req_get, req_get_flag);
-  return http_req_get;
+//  body_write_buffer_.clear();
+//  body_write_buffer_.append(string(http_req_get->req->output_buffer));
+//  head_write_buffer_.clear();
+//  body_write_buffer_.append(string(http_req_get->req->output_headers));
+  response_code_ = http_req_get->req->response_code;
+  http_request_free(http_req_get, req_get_flag);
+  return true;
 }
 
 
 
 Client::Client()
     : response_code_(0) {
-  head_write_buffer_.reset(new Buffer(FLAGS_max_head_buffer_size));
-  body_write_buffer_.reset(new Buffer(FLAGS_max_body_buffer_size));
+//  head_write_buffer_.reset(new Buffer(FLAGS_max_head_buffer_size));
+//  body_write_buffer_.reset(new Buffer(FLAGS_max_body_buffer_size));
 
   Reset();
 }
 
-void Client::SetHttpMethod(HttpMethod type) {
-  method_ = type;
+//void Client::SetHttpMethod(HttpMethod type) {
+//  method_ = type;
+//}
+
+int Client::GetResponseCode() const {
+  return response_code_;
 }
 
 void Client::SetPostParams(const map<string, string>& m) {
@@ -334,11 +347,11 @@ void Client::SetPostData(const string& data) {
 Client::~Client() {}
 
 const string& Client::ResponseHeader() const {
-  return head_write_buffer_->data;
+  return head_write_buffer_;
 }
 
 const string& Client::ResponseBody() const {
-  return body_write_buffer_->data;
+  return body_write_buffer_;
 }
 
 
@@ -361,25 +374,36 @@ void Client::SetFetchTimeout(int time_ms) {
 
 
 bool Client::IsHeaderTooLarge() const {
-  return head_write_buffer_->is_full;
+  //return head_write_buffer_->is_full;
+  return false;
 }
 
 bool Client::IsBodyTooLarge() const {
-  return body_write_buffer_->is_full;
+  //return body_write_buffer_->is_full;
+  return false;
 }
 
 void Client::Reset() {
   response_code_ = 0;
-  head_write_buffer_->Reset();
-  body_write_buffer_->Reset();
-
-  method_ = HttpMethod::kGet;
   post_data_.clear();
 }
 
-bool Client::FetchUrl(const string& url) {
+bool Client::FetchPostUrl(const string& url) {
+  server::EventBaseLoop event_loop;
   start_http_requset(event_loop.base(), url.c_str(),
-  method_,HTTP_CONTENT_TYPE_URL_ENCODED, "name=winlin&code=1234");
+    EVHTTP_REQ_POST,HTTP_CONTENT_TYPE_URL_ENCODED, post_data_.c_str());
+  event_loop.Dispatch();
+  event_base_free(event_loop.base());
+  return true;
+}
+
+bool Client::FetchGetUrl(const string& url) {
+  server::EventBaseLoop event_loop;
+  start_http_requset(event_loop.base(), url.c_str(),
+         EVHTTP_REQ_GET, NULL, NULL);
+  event_loop.Dispatch();
+  event_base_free(event_loop.base());
+  return true;
 }
 _END_CLIENT_NAMESPACE_  // namespace CLIENT
 
--- src/client.h
+++ src/client.h
@@ -21,24 +21,28 @@
 #include "base/compat.h"
 #include "base/hash_tables.h"
 #include "src/event_base_loop.h"
-
+#include "thirdlibs/event/include/event2/event.h"
+#include "thirdlibs/event/include/event2/buffer.h"
+#include "thirdlibs/event/include/event2/http.h"
+#include "thirdlibs/event/include/event2/http_struct.h"
+#include "thirdlibs/event/include/event2/keyvalq_struct.h"
 _START_CLIENT_NAMESPACE_
 
 class Buffer;
 struct http_request_get;
 struct http_request_post;
 
-enum HttpMethod {
-  EVHTTP_REQ_GET     = 1 << 0,
-  EVHTTP_REQ_POST    = 1 << 1,
-  EVHTTP_REQ_HEAD    = 1 << 2,
-  EVHTTP_REQ_PUT     = 1 << 3,
-  EVHTTP_REQ_DELETE  = 1 << 4,
-  EVHTTP_REQ_OPTIONS = 1 << 5,
-  EVHTTP_REQ_TRACE   = 1 << 6,
-  EVHTTP_REQ_CONNECT = 1 << 7,
-  EVHTTP_REQ_PATCH   = 1 << 8
-};
+//enum HttpMethod {
+//  EVHTTP_REQ_GET     = 1 << 0,
+//  EVHTTP_REQ_POST    = 1 << 1,
+//  EVHTTP_REQ_HEAD    = 1 << 2,
+//  EVHTTP_REQ_PUT     = 1 << 3,
+//  EVHTTP_REQ_DELETE  = 1 << 4,
+//  EVHTTP_REQ_OPTIONS = 1 << 5,
+//  EVHTTP_REQ_TRACE   = 1 << 6,
+//  EVHTTP_REQ_CONNECT = 1 << 7,
+//  EVHTTP_REQ_PATCH   = 1 << 8
+//};
 
 class Client {
  public:
@@ -46,13 +50,14 @@ class Client {
   ~Client();
 
   //  Call these functions after Reset().
-  void SetHttpMethod(HttpMethod type);
+//  void SetHttpMethod(HttpMethod type);
   // Join map to format like "k1=v1&k2=v2"
   void SetPostParams(const map<string, string>& m);
   void SetPostData(const string& data);
 
   void Reset();
-  bool FetchUrl(const string& url);
+  bool FetchGetUrl(const string& url);
+  bool FetchPostUrl(const string& url);
 
   int response_code() const {
     return response_code_;
@@ -65,6 +70,8 @@ class Client {
   bool IsHeaderTooLarge() const;
   bool IsBodyTooLarge() const;
 
+  int GetResponseCode() const;
+
   // Sets header.
   void AddHeader(const string& key, const string& value);
 
@@ -74,11 +81,11 @@ class Client {
   void SetProxy(const string& proxy_host, int proxy_port);
 
  private:
-  void print_request_head_info(struct evkeyvalq *header);
-  void print_uri_parts_info(const struct evhttp_uri * http_uri);
-  void http_requset_post_cb(struct evhttp_request *req, void *arg);
-  void http_requset_get_cb(struct evhttp_request *req, void *arg);
-  int start_url_request(struct http_request_get *http_req, int req_get_flag);
+//  void print_request_head_info(struct evkeyvalq *header);
+//  void print_uri_parts_info(const struct evhttp_uri * http_uri);
+//  void http_requset_post_cb(struct evhttp_request *req, void *arg);
+//  void http_requset_get_cb(struct evhttp_request *req, void *arg);
+//  int start_url_request(struct http_request_get *http_req, int req_get_flag);
   void *http_request_new(struct event_base* base,
                          const char *url,
                          int req_get_flag,
@@ -86,18 +93,18 @@ class Client {
                          const char* data);
   void http_request_free(struct http_request_get *http_req_get,
                          int req_get_flag);
-  void *start_http_requset(struct event_base* base,
+  bool start_http_requset(struct event_base* base,
                            const char *url,
                            int req_get_flag,
                            const char *content_type,
                            const char* data);
 
-  long response_code_;  // NOLINT
-  std::unique_ptr<Buffer> head_write_buffer_;
-  std::unique_ptr<Buffer> body_write_buffer_;
-  HttpMethod method_;
+  int response_code_;  // NOLINT
+  string  head_write_buffer_;
+  string  body_write_buffer_;
+//  HttpMethod method_;
   string post_data_;
-  server::EventBaseLoop event_loop;
+  //server::EventBaseLoop event_loop;
   DISALLOW_COPY_AND_ASSIGN(Client);
 };
 _END_CLIENT_NAMESPACE_  // namespace client
